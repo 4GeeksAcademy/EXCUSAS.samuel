429b85670b84d5bf40ec9a114cd3beb21a31a8e9
Jsamuel1919 <156345975+Jsamuel1919@users.noreply.github.com>
2024-02-21 18:43:36 +0000

    1commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,36 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let quien = [
+    "El perro",
+    "El gato",
+    "La cacatua",
+    "El perico",
+    "El loro",
+    "El hamster"
+  ];
+  let verbo = ["comio", "rompio", "Destrozo", "trago", "mordio"];
+  let que = ["La tarea", "el tabajo", "El proyecto", "La maqueta", "El folio"];
+  let cuando = ["Ayer", "Anoche", "Hace 5 min", "En la mañana", "No lo se"];
+
+  function generadoExcusas(quien, verbo, que, cuando) {
+    let Actquien = quien[Math.floor(Math.random() * quien.length)];
+
+    let Actverbo = verbo[Math.floor(Math.random() * verbo.length)];
+
+    let Actque = que[Math.floor(Math.random() * que.length)];
+
+    let Actcuando = cuando[Math.floor(Math.random() * cuando.length)];
+
+    return Actquien + " " + Actverbo + " " + Actque + " " + Actcuando;
+  }
+  console.log(generadoExcusas(quien, verbo, que, cuando));
+  function onLoad() {
+    console.log("prueba");
+    let excusaJose = document.getElementById("excuse");
+    excuse.innerHTML = generadoExcusas(quien, verbo, que, cuando);
+  }
+  window.onload = onLoad();
+
   console.log("Hello Rigo from the console!");
 };
